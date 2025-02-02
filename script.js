5ace0e640d5d260ca58ea0c0b096287eedde7b5c
Himanshu Raj <himanshuraj3696@.com>
2025-02-02 16:53:38 +0530

    second commit

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 let slides=document.querySelectorAll(".slides");
 
-let counter=0;
+let counterX=0;
 slides.forEach((slide,index)=>{
     slide.style.left=`${index*100}%`;
 })
 
-function slideImage(){
+function slideImageX(){
     slides.forEach((slide)=>{
-        slide.style.transform=`translateX(-${counter*100}%)`;
+        slide.style.transform=`translateX(-${counterX*100}%)`;
     })
 }
 
-// if you want to go behind first image and beyond last image
+// [During X translation] if you want to go behind first image and beyond last image
 function goPrev(){
-    counter--;
-    if(counter<0){
-        counter=slides.length-1;
+    counterX--;
+    if(counterX<0){
+        counterX=slides.length-1;
     }
-    slideImage();
+    slideImageX();
 }
 
 function goNext(){
-    counter++;
-    if(counter>slides.length-1){
-        counter=0;
+    counterX++;
+    if(counterX>slides.length-1){
+        counterX=0;
     }
-    slideImage();
+    slideImageX();
 }
 
-// when you don't want to go behind first image and beyond last image
+//  [During X translation] when you don't want to go behind first image and beyond last image
 // function goPrev(){
 //     if(counter>0){
 //         counter--;
@@ -41,4 +41,35 @@ function goNext(){
 //         counter++;
 //     }
 //     slideImage();
+// }
+
+
+// If you want to use top bottom buttons then use this
+// Please keep in mind that translation in X and translation in Y cannot co exist in this code so use one at a time
+// let counterY=0;
+
+// slides.forEach((slide,index)=>{
+//     slide.style.bottom=`${index*100}%`;
+// })
+
+// function slideImageY(){
+//     slides.forEach((slide)=>{
+//         slide.style.transform=`translateY(${counterY*100}%)`;
+//     })
+// }
+
+// function goUp(){
+//     counterY--;
+//     if(counterY<0){
+//         counterY=slides.length-1;
+//     }
+//     slideImageY();
+// }
+
+// function goDown(){
+//     counterY++;
+//     if(counterY>slides.length-1){
+//         counterY=0;
+//     }
+//     slideImageY();
 // }
